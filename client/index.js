dc9eafe4bd7942d1c9ac649449886299f5de82dd
Sahil Kamath <71258959+sahilkamath0108@users.noreply.github.com>
2025-07-13 23:07:55 +0530

    Add client/index.js

---
+++ client/index.js
@@ -0,0 +1,14 @@
+// client/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Netflix Clone</h1>
+      {/* Add your Netflix UI components here */}
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
